744f71b6a64bc3c8eca28f0e95cfc13699318663
romulo lima <dhb.tecnologia@gmail.com>
2021-09-02 19:00:42 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/io/zolthan31fps/resources/CategoriaResource.java
+++ src/main/java/io/zolthan31fps/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package io.zolthan31fps.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.zolthan31fps.domain.Categoria;
 
 
 @RestController
@@ -10,8 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
