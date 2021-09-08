c856fa9bb5a958c272adbd6d43e920d257e176fa
romulo lima <dhb.tecnologia@gmail.com>
2021-09-08 19:46:45 -0300

    Criando operacao de instanciacao

--- src/main/java/io/zolthan31fps/CursomcApplication.java
+++ src/main/java/io/zolthan31fps/CursomcApplication.java
@@ -1,13 +1,32 @@
 package io.zolthan31fps;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.zolthan31fps.domain.Categoria;
+import io.zolthan31fps.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Vestuario");
+		Categoria cat2 = new Categoria(null, "Acessorios");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
